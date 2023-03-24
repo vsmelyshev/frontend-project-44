03a5f066774e3b5234fc48ddec37c2152ef63ca1
vsmelyshev <vsmelyshev@gmail.com>
2023-03-25 01:32:37 +0300

    step 4 second part

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-console.log('node bin/brain-games.js\nWelcome to the Brain Games!');
 import readlineSync from 'readline-sync';
+
+console.log('node bin/brain-games.js\nWelcome to the Brain Games!');
 const name = readlineSync.question('May I have ypur name? ');
-console.log('Hello, ' + name + '!');
+console.log(`Hello, ${name}!`);

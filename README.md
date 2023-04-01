e4ab299200c6c7bae74033bc938f54ee42582124
vsmelyshev <vsmelyshev@gmail.com>
2023-04-01 14:35:16 +0300

    step 9 prime

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ https://asciinema.org/a/WHtcom18pGlnwUl9m5LhqyU7O
 https://asciinema.org/a/vWZkUtXH6jtNLEicjsVPXEVk6
 https://asciinema.org/a/b5fxDNEm83lgMWxmXlUxPCy99
 https://asciinema.org/a/two3P9BfL4qdbCdBk3H3viDRn
+https://asciinema.org/a/BupROsUgU8yY0EMSjmNZgqukU
---
+++ bin/brain-prime.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const primeCheck = (x) => {
+  let isPrime = true;
+  for (let i = 2; i < x; i += 1) {
+    if (x % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
+    return 'yes';
+  }
+  return 'no';
+};
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!\nAnswer "yes" if given number is prime. Otherwise answer "no".`);
+let n = 0;
+let result = 'no';
+while (n < 3) {
+  const number = Math.floor(Math.random() * 99) + 2;
+  console.log(`Question: ${number}`);
+  result = primeCheck(number);
+  const answer = readlineSync.question('Your answer: ');
+  if (answer === result) {
+    n += 1;
+    console.log('Correct!');
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.`);
+    break;
+  }
+}
+if (n === 3) {
+  console.log(`Congratulations, ${name}!`);
+}
